840956e10d014057ff272f43a7700bc5d1d81bec
Moridius <moridius-github@posteo.hu>
2017-07-28 22:47:16 +0200

    Basics.

--- nodesTk/nodesTk.py
+++ nodesTk/nodesTk.py
@@ -5,9 +5,13 @@ import json
 
 class Network():
     def __init__(self):
-        pass
+        self.nodes_dict = dict()
 
 
 class Node():
     def __init__(self):
         pass
+
+
+if __name__ == "__main__":
+    pass
